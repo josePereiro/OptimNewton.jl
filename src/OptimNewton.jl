0e5008b377fbbaca08e69f3f9079d67c74e48002
Jose Antonio Pereiro Morejon <jose.pereiro.lab@gmail.com>
2019-03-14 00:59:21 -0400

    First commit!!! Two function implemented get_root and optimize!!

--- src/OptimNewton.jl
+++ src/OptimNewton.jl
@@ -1,5 +1,46 @@
 module OptimNewton
 
-greet() = print("Hello World!")
+export get_root;
+export optimize;
+
+"""
+	Try to find a root of (fun). You need to provide the first derivative of fun (fder) and an initial x0 value.	Additional parameters are used as stop signals.	xtol, ytol: are the tolerances for x and f(x) respectively, if further iteration do not	improve the result in more that this tolerance values, the method returns. itn: is the max number of iterations that the method will do. If verbose is true, the method will print a status message in every iteration.
+"""
+function get_root(x0, fun, fder; xtol = 10e-10, ytol = 10e-10,
+ itn = 10, verbose = false)
+	
+	r = x0; 
+	lastr = r;
+	for i in 1:itn
+		verbose && print("iteration $i: initial root $r,");
+		r = r - fun(r)/fder(r);
+		verbose && println(" oprox root $r");
+		abs(lastr - r) <= xtol && break;
+		lastr = r;
+	end
+
+	return r;
+
+end # get_root
+
+"""
+	Try to find a optimus value of (fun). You need to provide the first derivative of fun (fder), de secund derivative of fun (sder) and an initial x0 value. Additional parameters are used as stop signals. xtol, ytol: are the tolerances for x and f(x) respectively, if further iteration do not	improve the result in more that this tolerance values, the method return. itn: is the max number of iterations that the method will do. If verbose is true, the method will print a status message in every iteration.
+"""
+function optimize(x0, fun, fder, sder; xtol = 10e-10, ytol = 10e-10, itn = 10, verbose = false)
+
+	r = x0; 
+	lastr = r;
+	for i in 1:itn
+		verbose && print("iteration $i: before value $r,");
+		r = r - fder(r)/sder(r);
+		verbose && println(" after value $r");
+		abs(lastr - r) <= xtol && break;
+		abs(fun(r) - fun(lastr)) <= ytol && break;
+		lastr = r;
+	end
+
+	return r;
+
+end # optimize
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using OptimNewton
 using Test
 
 @testset "OptimNewton.jl" begin
-    # Write your own tests here.
-end
+    # get_root.
+    # x0 = pi/10, f(x) = sin(x), fd(x) = cos(x).
+    @test isapprox(get_root(pi/10, x -> sin(x), x -> cos(x)), 0.0);
+    # x0 = 1, f(x) = 2 - x^2 + x^3, fd(x) = -2x + 3x^2.
+    @test isapprox(get_root(1, x -> 2 - x^2 + x^3, x -> -2x + 3x^2),-1);
+
+    # optimize
+    # x0 = pi/3, f(x) = sin(x), fd(x) = cos(x), sd(x) = -sin(x)
+    @test isapprox(optimize(pi/3, x -> sin(x), x -> cos(x), x -> -sin(x)), pi/2);
+    # x0 = 1, f(x) = 2 - x^2 + x^3, fd(x) = -2x + 3x^2, sd(x) = -2 + 6x.
+    @test isapprox(optimize(1, x -> 2 - x^2 + x^3, x -> -2x + 3x^2, x -> -2 + 6x),2/3);
+ end
